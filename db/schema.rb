e6922d39e1b6994c25433736ce2bdbf751c5d432
andychims <andychims@gmail.com>
2015-01-27 21:31:49 -0500

    added TextPosts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128015617) do
+ActiveRecord::Schema.define(version: 20150128023042) do
 
   create_table "follows", force: true do |t|
     t.integer  "user_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150128015617) do
   create_table "image_links", force: true do |t|
     t.string   "title"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "text_posts", force: true do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
